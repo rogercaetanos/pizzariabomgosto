2e05b0f945608ef5aef1f7abda373499b647f3b9
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2025-03-19 22:00:12 -0300

    Camada de Tratamento de Erros em Construção

---
+++ src/main/java/com/itb/mif3an/pizzariabomgosto/exceptions/BadRequest.java
@@ -0,0 +1,11 @@
+package com.itb.mif3an.pizzariabomgosto.exceptions;
+
+
+
+public class BadRequest extends RuntimeException {
+
+    public BadRequest(String message) {
+        // super: acessando o construtor da classe pai
+        super(message);
+    }
+}
